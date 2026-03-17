42adf6fbc926f2711fea3d907f3e3639670f21cf
JayVanilla <leaotuitamaj50371@masseyhigh.school.nz>
2026-03-18 10:42:30 +1300

    Input Output

---
+++ P_03_input_output.py
@@ -0,0 +1,20 @@
+# ask the user for their name
+username = input("What is your name? ")
+
+# ask the user for their favourite number
+fav_num = int(input("What is your favourite number? "))
+
+# Double, halve and square the user's favourite number
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+# Greet the user
+print(f"\nHi {username}, your favourite number is {fav_num} ")
+# Output the results of doubling, halving and
+print(f"Doubling your favourite number gives: {double} ")
+print(f"Halving your number gives: {halve} ")
+print(f"Squaring your number gives: {square} ")
+print()
+print("Have a radical day. (;")
+# squaring their favourite integer
