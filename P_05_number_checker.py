0506f0622b801c7487aa011c0e9b756ec64d83b2
JayVanilla <leaotuitamaj50371@masseyhigh.school.nz>
2026-03-18 10:54:13 +1300

    Working number checking function

---
+++ P_05_number_checker.py
@@ -0,0 +1,31 @@
+# Ask for width and loop until they
+# enter a number that is more than zero
+def num_check (question):
+
+    error = "Please enter a number that is more than zero, yes this is a human that wrote this dude\n"
+    while True:
+
+        try:
+            # ask the user for a number
+            response = float(input(question))
+
+            # check that the number is more than zero
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine Goes here
+for item in range(0, 2):
+    width = num_check("Width:  ")
+    print(width)
+
+    print()
+
+for item in range(0, 2):
+    height = num_check("Height:  ")
+    print(height)
